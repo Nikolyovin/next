e37f1912cde87218a013950ee3bf4311765cae01
Лёвин Николай Сергеевич <151705@grpz.su>
2023-01-23 15:53:38 +0300

    first commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Main from './Main/Main'
+
+const HomeContainer = () => {
+  return (
+    <div className=''>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default HomeContainer
