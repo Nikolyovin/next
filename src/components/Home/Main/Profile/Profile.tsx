d4dedeb391c6a5d317cf1aa383b4cbe61bf0b18f
Nikolyovin <pulya0763@gmail.com>
2023-01-29 19:40:24 +0300

    fix font

--- src/components/Home/Main/Profile/Profile.tsx
+++ src/components/Home/Main/Profile/Profile.tsx
@@ -29,13 +29,13 @@ const Profile = () => {
                     </div>
 
                     <Divider sx={{ width: '25%' }} />
-                    <p className='mt-2 text-slate-500 robotoCondensed amatic'>Рязань, Москва, rtvth</p>
+                    <h6 className='mt-2 text-slate-500 robotoCondensed amatic'>Рязань, Москва, rtvth</h6>
                     {/* <div className='flex justify-between'> */}
-                    <p className='mt-3 mb-2 text-slate-500 FontText'>
+                    <span className='mt-3 mb-2 text-slate-500 FontText'>
                         Привет! Меня зовут Виктория и я мастер тату. Также являюсь членом Ассоциации Художников
                         Ботанического Искусства. Я очень люблю ботанику, и через свои работы чувствую гармонию с
                         природой.
-                    </p>
+                    </span>
                     <p className='mt-3 mb-6 text-slate-500 FontText'>
                         Люблю создавать красоту на теле своих клиентов и заряжать их своей энергией.Мои любимые стили -
                         это ботаника и микрореализм. Если вы хотите добавить что-то особенное в свою жизнь, давайте
